53d536fd6a6d8e8fb5462cf01d08c44a3baa714a
Jean Gabriel <jeangabriel180@hotmail.com>
2024-02-29 22:43:59 -0300

    primerio commit

---
+++ src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,54 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Serie extends Titulo {
+    private int temporadas;
+    private boolean ativa;
+    private int episodioPorTemporada;
+    private int minutosPorEpisodio;
+
+    public Serie(String nome, int anoDeLancamento) {
+        super(nome, anoDeLancamento);
+    }
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpisodioPorTemporada() {
+        return episodioPorTemporada;
+    }
+
+    public void setEpisodioPorTemporada(int episodioPorTemporada) {
+        this.episodioPorTemporada = episodioPorTemporada;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporadas * episodioPorTemporada * minutosPorEpisodio;
+    }
+
+    @Override
+    public String toString() {
+        return "Serie: " + this.getNome() + "(" + this.getAnoDeLancamento() + ")";
+    }
+}
